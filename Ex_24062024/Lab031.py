b92ada3830a4d976416d7a97f0062e6cd96b8a48
Veeresh <veereshmtl@gmail.com>
2024-06-23 15:20:11 +0530

    Add the items using append, insert and extended

--- Ex_24062024/Lab031.py
+++ Ex_24062024/Lab031.py
@@ -1,5 +1,8 @@
+# Change list item
 fruits = ["apple", "banana", "cherry","pineapple","mango","pappaya"]
 print(fruits)
 fruits[1] = "kiwi"
+print(fruits)
 
+fruits.insert(2,"pineapple")
 print(fruits)
---
+++ Ex_24062024/Lab032.py
@@ -0,0 +1,18 @@
+# Add list items using the append() method
+thislist = ["apple", "banana", "cherry"]
+#To add an item to the end of the list, use the append() method:
+
+thislist.append("orange")
+print(thislist)
+
+# Insert method
+thislist.insert(1,"kiwi")
+print(thislist)
+# adding the item using index
+thislist[1]="mango"
+print(thislist)
+
+#Extended method
+
+thislist.extend(["pappaya","grapes"])
+print(thislist)
